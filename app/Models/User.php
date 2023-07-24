8ac086a977684b18119eaa0a4559f3b65538537e
Carlo Bactol <74802803+CarloBactol@users.noreply.github.com>
2023-07-24 03:10:51 +0000

    Add AuthController

--- app/Models/User.php
+++ app/Models/User.php
@@ -20,8 +20,10 @@ class User extends Authenticatable
     protected $fillable = [
         'name',
         'email',
+        'image',
         'password',
     ];
+
 
     /**
      * The attributes that should be hidden for serialization.
